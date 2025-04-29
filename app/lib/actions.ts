b674d760026872ac0850a164e67f412fe0aa9866
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2025-04-29 17:56:00 +0500

    add server side form validation on update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,13 +64,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 };
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -82,7 +91,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     console.error(error);
   }
-
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
